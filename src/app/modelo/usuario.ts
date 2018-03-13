4e0aa92e2287b4a110da265b799235d6ba0a279a
Luis Miguel Rubio <luis-miguel.rubio@soprasteria.com>
2018-03-13 16:08:01 +0100

    errores

--- src/app/modelo/usuario.ts
+++ src/app/modelo/usuario.ts
@@ -1,10 +1,11 @@
-export class Usuario{
+export class Usuario {
     constructor(
         public id: number,
         public nick: string,
         public nombre: string,
+        public apellidos: string,
         public pass: string
-    ){
-        
+    ) {
+
     }
 }
